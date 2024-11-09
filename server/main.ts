039fb9caadbdb2faaf941c567256eeb4066ac947
Peter Baričič <peter@vestberry.com>
2024-11-09 22:29:51 +0100

    move to graphql-yoga and pothos-graphql

--- server/main.ts
+++ server/main.ts
@@ -1,44 +1,28 @@
-import { GraphQLHTTP } from "jsr:@deno-libs/gql";
+import { createYoga } from "npm:graphql-yoga@4.0.4";
+import { schema } from "./schema.ts";
 
-import schema from "./schema.ts";
+// Create Yoga server
+const yoga = createYoga({
+  schema,
+  graphiql: true,
+  cors: {
+    origin: "*",
+    credentials: true,
+    methods: ["POST", "GET", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  },
+  fetchAPI: {
+    Request: Request,
+    Response: Response,
+  },
+});
 
-function setCORS(response: Response): Response {
-  const headers = new Headers(response.headers);
-  headers.set("Access-Control-Allow-Origin", "*");
-  headers.set(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS",
-  );
-  headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  return new Response(response.body, {
-    status: response.status,
-    statusText: response.statusText,
-    headers,
-  });
-}
-
-const handler = async (req: Request) => {
-  if (req.method === "OPTIONS") {
-    return setCORS(new Response(null));
-  }
-
-  const { pathname } = new URL(req.url);
-
-  const response =
-    pathname === "/graphql"
-      ? await GraphQLHTTP<Request>({
-          schema,
-          graphiql: true,
-        })(req)
-      : new Response("Not Found", { status: 404 });
-
-  return setCORS(response);
-};
-
+// Start server using Deno
 Deno.serve(
   {
     port: 8000,
   },
-  handler,
+  yoga,
 );
+
+console.log(`🚀 Server running at http://localhost:8000/graphql`);
